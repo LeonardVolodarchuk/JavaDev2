f1591cf47e5d37367d3372469b197102a2c93113
LeonardVolodarchuk <126458141+LeonardVolodarchuk@users.noreply.github.com>
2023-08-07 19:23:11 +0300

    Initial commit

---
+++ src/main/java/goit/test/CalculatorTest.java
@@ -0,0 +1,50 @@
+package goit.test;
+
+import goit.calculator.SimpleCalculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    private SimpleCalculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new SimpleCalculator();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    public void testAdd() {
+        int result = calculator.add(3, 5);
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        int result = calculator.subtract(5, 3);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        int result = calculator.multiply(2, 5);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testDivide() {
+        int result = calculator.divide(12, 3);
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(10, 0));
+    }
+}
